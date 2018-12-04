636f83c453286827e6f627b82b7b06ab5676cacb
fancyweb <calvet.thomas@gmail.com>
2018-12-04 14:44:59 +0100

    test(exception): add unit tests

---
+++ tests/Unit/src/Exception/NotImplementedExceptionTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Ekino\Drupal\Debug\Tests\Unit\Exception;
+
+use Ekino\Drupal\Debug\Exception\NotImplementedException;
+use PHPUnit\Framework\TestCase;
+
+class NotImplementedExceptionTest extends TestCase
+{
+    /**
+     * @var NotImplementedException
+     */
+    private $notImplementedException;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
+    {
+        $this->notImplementedException = new NotImplementedException('foo');
+    }
+
+    public function testInstanceOfException()
+    {
+        $this->assertInstanceOf(\Exception::class, $this->notImplementedException);
+    }
+
+    public function testGetMessage()
+    {
+        $this->assertSame('foo', $this->notImplementedException->getMessage());
+    }
+}
