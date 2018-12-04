62f01b62f7b4e39327f1ffb18dad8a92b089f09a
fancyweb <calvet.thomas@gmail.com>
2018-12-04 14:15:08 +0100

    test(settings-helper): add unit tests

---
+++ tests/Unit/src/Helper/SettingsHelperTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Ekino\Drupal\Debug\Tests\Unit\Kernel;
+
+use Drupal\Core\Site\Settings;
+use Ekino\Drupal\Debug\Helper\SettingsHelper;
+use PHPUnit\Framework\TestCase;
+
+class SettingsHelperTest extends TestCase
+{
+    /**
+     * @var bool
+     */
+    protected $runTestInSeparateProcess = true;
+
+    /**
+     * @var SettingsHelper
+     */
+    private $settingsHelper;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
+    {
+        $this->settingsHelper = new SettingsHelper();
+    }
+
+    public function testOverrideOnExistingKey()
+    {
+        new Settings(array(
+            'foo' => 'bar',
+            'one' => array(
+                'two' => array(
+                    'three' => 'val',
+                ),
+                'must' => 'not change',
+            ),
+        ));
+
+        $this->settingsHelper->override('[foo]', 'ccc');
+        $this->assertSame('ccc', Settings::get('foo'));
+
+        $this->settingsHelper->override('[one][two][three]', 'php');
+        $this->assertSame(array(
+              'two' => array(
+                    'three' => 'php',
+              ),
+              'must' => 'not change',
+        ), Settings::get('one'));
+    }
+
+    public function testOverrideOnNewKey()
+    {
+        new Settings(array());
+
+        $this->settingsHelper->override('[foo]', 'ccc');
+        $this->assertSame('ccc', Settings::get('foo'));
+
+        $this->settingsHelper->override('[one][two]', array(
+            'foo' => 'php',
+        ));
+        $this->assertSame(array(
+            'two' => array(
+                'foo' => 'php',
+            ),
+        ), Settings::get('one'));
+    }
+}
