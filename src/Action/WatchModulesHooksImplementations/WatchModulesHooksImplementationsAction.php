447549095b92e9bcfe3b3e602b9399c51855a938
brambaud <beram.job@gmail.com>
2019-02-07 09:16:15 +0100

    fix(watch-modules-hooks-implementations): remove circular reference

--- src/Action/WatchModulesHooksImplementations/WatchModulesHooksImplementationsAction.php
+++ src/Action/WatchModulesHooksImplementations/WatchModulesHooksImplementationsAction.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Ekino\Drupal\Debug\Action\WatchModulesHooksImplementations;
 
+use Drupal\Core\DrupalKernelInterface;
 use Drupal\Core\Extension\ModuleHandler;
 use Ekino\Drupal\Debug\Action\ActionWithOptionsInterface;
 use Ekino\Drupal\Debug\Action\CompilerPassActionInterface;
@@ -21,13 +22,13 @@ use Ekino\Drupal\Debug\Action\ValidateContainerDefinitionTrait;
 use Ekino\Drupal\Debug\Cache\Event\FileBackendEvents;
 use Ekino\Drupal\Debug\Cache\FileBackend;
 use Ekino\Drupal\Debug\Cache\FileCache;
+use Ekino\Drupal\Debug\Exception\NotSupportedException;
 use Ekino\Drupal\Debug\Kernel\Event\AfterAttachSyntheticEvent;
 use Ekino\Drupal\Debug\Kernel\Event\DebugKernelEvents;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class WatchModulesHooksImplementationsAction implements CompilerPassActionInterface, EventSubscriberActionInterface, ActionWithOptionsInterface
 {
@@ -37,6 +38,11 @@ class WatchModulesHooksImplementationsAction implements CompilerPassActionInterf
      * @var string
      */
     private const RESOURCES_SERVICE_ID = 'ekino.drupal.debug.action.watch_modules_hooks_implementations.resources';
+
+    /**
+     * @var string
+     */
+    private const EVENT_DISPATCHER_SERVICE_ID = 'ekino.drupal.debug.action.watch_modules_hooks_implementations.event_dispatcher';
 
     /**
      * @var WatchModulesHooksImplementationsOptions
@@ -49,7 +55,7 @@ class WatchModulesHooksImplementationsAction implements CompilerPassActionInterf
     public static function getSubscribedEvents(): array
     {
         return array(
-            DebugKernelEvents::AFTER_ATTACH_SYNTHETIC => 'setResources',
+            DebugKernelEvents::AFTER_ATTACH_SYNTHETIC => array(array('setResources'), array('addListener')),
         );
     }
 
@@ -67,8 +73,6 @@ class WatchModulesHooksImplementationsAction implements CompilerPassActionInterf
     public function process(ContainerBuilder $container): void
     {
         $moduleHandlerDefinition = $this->validateContainerDefinitionClassIs($container, 'module_handler', ModuleHandler::class);
-        $eventDispatcherDefinition = $this->validateContainerDefinitionClassImplements($container, 'event_dispatcher', EventDispatcherInterface::class);
-        $this->validateContainerDefinitionClassImplements($container, 'kernel', HttpKernelInterface::class);
 
         $resourcesDefinition = new Definition();
         $resourcesDefinition->setSynthetic(true);
@@ -80,20 +84,16 @@ class WatchModulesHooksImplementationsAction implements CompilerPassActionInterf
                 new Reference(self::RESOURCES_SERVICE_ID),
             )),
         ));
+
+        $eventDispatcherDefinition = new Definition();
+        $eventDispatcherDefinition->setSynthetic(true);
+        $container->setDefinition(self::EVENT_DISPATCHER_SERVICE_ID, $eventDispatcherDefinition);
+
         $fileBackendDefinition->addMethodCall('setEventDispatcher', array(
-            new Reference('event_dispatcher'),
+            new Reference(self::EVENT_DISPATCHER_SERVICE_ID),
         ));
 
         $moduleHandlerDefinition->replaceArgument(2, $fileBackendDefinition);
-
-        $eventDispatcherDefinition
-            ->addMethodCall('addListener', array(
-                FileBackendEvents::ON_CACHE_NOT_FRESH,
-                new Definition(LoadNewModuleFile::class, array(
-                    new Reference('module_handler'),
-                    new Reference('kernel'),
-                )),
-          ));
     }
 
     /**
@@ -102,6 +102,35 @@ class WatchModulesHooksImplementationsAction implements CompilerPassActionInterf
     public function setResources(AfterAttachSyntheticEvent $event): void
     {
         $event->getContainer()->set(self::RESOURCES_SERVICE_ID, $this->options->getFilteredResourcesCollection($event->getEnabledModules(), $event->getEnabledThemes()));
+    }
+
+    /**
+     * @param AfterAttachSyntheticEvent $event
+     *
+     * @throws NotSupportedException
+     */
+    public function addListener(AfterAttachSyntheticEvent $event): void
+    {
+        $eventDispatcher = new EventDispatcher();
+        $event->getContainer()->set(self::EVENT_DISPATCHER_SERVICE_ID, $eventDispatcher);
+
+        $moduleHandler = $event->getContainer()->get('module_handler');
+        if (!$moduleHandler instanceof ModuleHandler) {
+            throw new NotSupportedException(\sprintf('The "module_handler" service class should be "%s".', ModuleHandler::class));
+        }
+
+        $kernel = $event->getContainer()->get('kernel');
+        if (!$kernel instanceof DrupalKernelInterface) {
+            throw new NotSupportedException(\sprintf('The "kernel" service class should implement the "%s" interface.', DrupalKernelInterface::class));
+        }
+
+        $eventDispatcher->addListener(
+            FileBackendEvents::ON_CACHE_NOT_FRESH,
+            new LoadNewModuleFile(
+                $moduleHandler,
+                $kernel
+            )
+        );
     }
 
     /**
